cd5a6f33d71a24c66e2250292e1bdf972a383bc7
kentrino <kentrino@h-kento.jp>
2017-02-03 18:02:53 +0900

    wip

--- src/component/test.jsx
+++ src/component/test.jsx
@@ -1,7 +1,8 @@
+// @flow
 import style from './Test.css';
 
-const test = {
-  render (h) {
+const test: ComponentOptions = {
+  render (h: any): VNode {
     return <div><h1 class={style.red}>Yeah!! This is test.</h1></div>
   },
   name: 'Test'
